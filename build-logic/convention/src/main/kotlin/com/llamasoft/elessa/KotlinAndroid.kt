ff4c31b9c4f9069153f559a09c6e62177a1f046f
jaegerfox <kevin.edgard.perez@gmail.com>
2025-05-21 14:15:11 -0500

    build.gradle update

--- build-logic/convention/src/main/kotlin/com/llamasoft/elessa/KotlinAndroid.kt
+++ build-logic/convention/src/main/kotlin/com/llamasoft/elessa/KotlinAndroid.kt
@@ -3,9 +3,6 @@ package com.llamasoft.elessa
 import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.JavaVersion
 import org.gradle.api.Project
-import org.gradle.api.plugins.ExtensionAware
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.dsl.KotlinJvmOptions
 
 internal fun Project.configureKotlinAndroid(commonExtension: CommonExtension<*, *, *, *, *, *>) {
     commonExtension.apply {
@@ -13,30 +10,15 @@ internal fun Project.configureKotlinAndroid(commonExtension: CommonExtension<*, 
 
         defaultConfig {
             minSdk = 21
-
             testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-            vectorDrawables {
-                useSupportLibrary = true
-            }
+            vectorDrawables.useSupportLibrary = true
         }
 
         compileOptions {
-            sourceCompatibility(JavaVersion.VERSION_17)
-            targetCompatibility(JavaVersion.VERSION_17)
+            sourceCompatibility = JavaVersion.VERSION_21
+            targetCompatibility = JavaVersion.VERSION_21
         }
 
-        kotlinOptions {
-            jvmTarget = JvmTarget.JVM_17.target
-        }
-
-        packaging {
-            resources {
-                excludes.add("/META-INF/{AL2.0,LGPL2.1}")
-            }
-        }
+        packaging.resources.excludes.add("/META-INF/{AL2.0,LGPL2.1}")
     }
-}
-
-private fun CommonExtension<*, *, *, *, *, *>.kotlinOptions(block: KotlinJvmOptions.() -> Unit) {
-    (this as ExtensionAware).extensions.configure("kotlinOptions", block)
 }
