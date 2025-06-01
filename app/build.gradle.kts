1db14e18d7cbf3577447801ba315a41dde492b98
kevin.perez5 <kevin.perez@naranjax.com>
2025-06-01 00:34:05 -0500

    feat: support build variants and flavors for dev, stage, and prod

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,6 @@
+import com.android.build.gradle.internal.api.BaseVariantOutputImpl
+import java.util.Properties
+
 plugins {
     id("llamasoft.android.application")
     id("llamasoft.android.compose")
@@ -5,20 +8,68 @@ plugins {
     id("llamasoft.koin")
 }
 
+val versionProps = Properties().apply {
+    load(rootProject.file("delivery/version.properties").inputStream())
+}
+
+val major = versionProps["MAJOR"].toString().toInt()
+val minor = versionProps["MINOR"].toString().toInt()
+val patch = versionProps["PATCH"].toString().toInt()
+val code = versionProps["CODE"].toString().toInt()
+
+val versionName = "$major.$minor.$patch"
+val versionCode = code
+
+val appBaseName = "Elessa"
+
 android {
     namespace = "com.llamasoft.elessa"
 
     defaultConfig {
         applicationId = "com.llamasoft.elessa"
+        this.versionCode = versionCode
+        this.versionName = versionName
+    }
+
+    flavorDimensions += "environment"
+
+    productFlavors {
+        create("dev") {
+            dimension = "environment"
+            applicationIdSuffix = ".dev"
+            versionNameSuffix = "-dev"
+            resValue("string", "app_name", "\"$appBaseName dev $versionName\"")
+            resValue("color", "launcher_color", "#7f8c8d")
+        }
+        create("stage") {
+            dimension = "environment"
+            applicationIdSuffix = ".stage"
+            versionNameSuffix = "-stage"
+            resValue("string", "app_name", "\"$appBaseName stage $versionName\"")
+            resValue("color", "launcher_color", "#2980b9")
+        }
+        create("prod") {
+            dimension = "environment"
+            resValue("string", "app_name", "\"$appBaseName\"")
+        }
     }
 
     buildTypes {
-        debug {
+        getByName("debug") {
             enableUnitTestCoverage = true
         }
-        release {
+        getByName("release") {
             isMinifyEnabled = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    applicationVariants.all {
+        outputs.all {
+            val output = this as BaseVariantOutputImpl
+            val flavor = flavorName
+            val buildType = buildType.name
+            output.outputFileName = "elessa-$flavor-$buildType-v$versionName.apk"
         }
     }
 
@@ -35,6 +86,7 @@ dependencies {
     implementation(project(":core:network"))
     implementation(project(":core:navigation"))
     implementation(project(":elessa-ui"))
+
     // Home
     implementation(project(":home:data"))
     implementation(project(":home:domain"))
@@ -47,7 +99,6 @@ dependencies {
     implementation(project(":checkout:presentation"))
 
     implementation(libs.androidx.lifecycleRuntime.ktx)
-
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.analytics)
 }
